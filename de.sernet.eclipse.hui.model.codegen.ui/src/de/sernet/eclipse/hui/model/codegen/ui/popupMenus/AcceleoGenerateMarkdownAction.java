45989e75f7012eac6b0997aca086d00b887016cd
Urs Zeidler <uz@sernet.de>
2019-03-20 17:17:26 +0100

    restructure mtl templates and add new starter

---
+++ de.sernet.eclipse.hui.model.codegen.ui/src/de/sernet/eclipse/hui/model/codegen/ui/popupMenus/AcceleoGenerateMarkdownAction.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package de.sernet.eclipse.hui.model.codegen.ui.popupMenus;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionDelegate;
+
+import de.sernet.eclipse.hui.model.codegen.main.GenerateMarkdown;
+import de.sernet.eclipse.hui.model.codegen.ui.Activator;
+
+/**
+ * Codegen code generation.
+ */
+public class AcceleoGenerateMarkdownAction extends ActionDelegate implements IActionDelegate {
+	
+	/**
+	 * Selected model files.
+	 */
+	protected List<IFile> files;
+
+	/**{@inheritDoc}
+	 *
+	 * @see org.eclipse.ui.actions.ActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			files = ((IStructuredSelection) selection).toList();
+		}
+	}
+
+	/**{@inheritDoc}
+	 *
+	 * @see org.eclipse.ui.actions.ActionDelegate#run(org.eclipse.jface.action.IAction)
+	 * @generated
+	 */
+	public void run(IAction action) {
+		if (files != null) {
+			IRunnableWithProgress operation = new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor) {
+					try {
+						Iterator<IFile> filesIt = files.iterator();
+						while (filesIt.hasNext()) {
+							IFile model = (IFile)filesIt.next();
+							URI modelURI = URI.createPlatformResourceURI(model.getFullPath().toString(), true);
+							IContainer targetFolder = null;
+							try {
+								IContainer target = model.getProject().getFolder("src-gen");
+								targetFolder = target;
+								
+								if (!targetFolder.getLocation().toFile().exists()) {
+									targetFolder.getLocation().toFile().mkdirs();
+								}
+								
+								monitor.subTask("Loading...");
+								GenerateMarkdown gen0 = 
+										new de.sernet.eclipse.hui.model.codegen.main.GenerateMarkdown(
+												modelURI, targetFolder.getLocation().toFile(), getArguments());
+								monitor.worked(1);
+								String generationID = org.eclipse.acceleo.engine.utils.AcceleoLaunchingUtil.computeUIProjectID("de.sernet.eclipse.hui.model.codegen", "de.sernet.eclipse.hui.model.codegen.main.GenerateHuiModelReport", modelURI.toString(), targetFolder.getFullPath().toString(), new ArrayList<String>());
+								gen0.setGenerationID(generationID);
+								gen0.doGenerate(BasicMonitor.toMonitor(monitor));
+
+								
+//								
+//								GenerateAll generator = new GenerateAll(modelURI, targetFolder, getArguments());
+//								generator.doGenerate(monitor);
+							} catch (IOException e) {
+								IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+								Activator.getDefault().getLog().log(status);
+							} finally {
+								if (targetFolder != null) {
+									targetFolder.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+								}
+							}
+						}
+					} catch (CoreException e) {
+						IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+						Activator.getDefault().getLog().log(status);
+					}
+				}
+			};
+			try {
+				PlatformUI.getWorkbench().getProgressService().run(true, true, operation);
+			} catch (InvocationTargetException e) {
+				IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+				Activator.getDefault().getLog().log(status);
+			} catch (InterruptedException e) {
+				IStatus status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+				Activator.getDefault().getLog().log(status);
+			}
+		}
+	}
+
+	/**
+	 * Computes the arguments of the generator.
+	 * 
+	 * @return the arguments
+	 * @generated
+	 */
+	protected List<? extends Object> getArguments() {
+		return new ArrayList<String>();
+	}
+
+}
